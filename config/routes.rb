1042b3d4ff94b6f581ae64c75fb45d12d351a4da
Thiago-Cardoso <trovador182@gmail.com>
2019-01-06 20:27:09 -0200

    Adjustment in Layouts, dinamic layout and put chat using angular

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :match
-  resources :sugestions
-  resources :dashboard
+  resources :combinations
   resources :information_types
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
-  root 'landing_page#index'
+  root 'dashboard#index'
+  get 'tags/:tag', to: 'users#index', as: :tag
+  resources :dashboard, only: [:index, :show]
+  resources :sugestions, only: [:show]
 end
