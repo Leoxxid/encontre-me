d9855ec10a7251a96912b3325d02dbc2806e7bd1
Tiago Leal <tiago.sleal12@gmail.com>
2019-01-07 00:21:20 -0200

    fix chat

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,7 @@ class DashboardController < ApplicationController
     @user = User.find(current_user.id)
     @users = User.all
     params[:tag] ? @users = User.tagged_with(params[:tag]) : @users = User.all
-    #@user_id = current_user.id
-    #user_information_types =UserInformationType.where(user_id: current_user.id).destroy_all
+    @user_information_types = UserInformationType.where(user_id: current_user.id) 
   end
 
   def show
--- app/models/information_type.rb
+++ app/models/information_type.rb
@@ -1,7 +1,7 @@
 class InformationType < ApplicationRecord
   # validates :description, presence: true
   has_many :user_information_types, dependent: :destroy
-  belongs_to :user, required: false
+  # belongs_to :user, required: false
 
   def name_with_initial
     "#{description}"
